15e24caf32ade0ef780ab6ec0cb01ef97b389f92
조용일 <cyi1207@naver.com>
2021-07-15 15:26:10 +0900

    real final

--- frontend/src/views/error/Error.vue
+++ frontend/src/views/error/Error.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container" style="text-aligh: center">
+    <div class="container" style="text-align: center">
         <h1><strong>Error!!</strong></h1>
         <h3>The network connection seems to be the problem.</h3>
     </div>
