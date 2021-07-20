024aef6c774213c0f6ef1e68adc1f71ecb0996df
조성표 <pyo7410@gmail.com>
2021-07-20 17:01:01 +0900

    feat: rid로 Reply를 검색하는 기능 추가
    2021.07.20
    - JPA를 사용한 검색을 위해 ReplyDao -> findReplyByRid 추가
    
    #S05P12C203-37

--- backend/src/main/java/com/web/curation/dao/reply/ReplyDao.java
+++ backend/src/main/java/com/web/curation/dao/reply/ReplyDao.java
@@ -3,5 +3,8 @@ package com.web.curation.dao.reply;
 import com.web.curation.model.reply.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReplyDao extends JpaRepository<Reply, String> {
+    Optional<Reply> findReplyByRid(Long rid);
 }
