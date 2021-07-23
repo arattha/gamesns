81acd36d86ca02bc7708c20d7843f6850a995724
성루비 <quipu1@naver.com>
2021-07-23 17:29:44 +0900

    feat: Frontend 팔로잉 리스트
    2021.07.23 팔로워 리스트 백엔드와의 통신 로직 구현
    팔로잉 목록 출력 구현
    
    #S05P12C203-94

--- frontend/src/views/user/Following.vue
+++ frontend/src/views/user/Following.vue
@@ -1,38 +1,53 @@
 <template>
   <div>
+    <Header/>
+    <div>
       <h1>팔로잉 리스트</h1>
-      <!-- v-for로 리스트 -->
-      <!-- 일단,, 대충 ㅣㅇ런 느낌으로다가..,. -->
-      <ul v-for="follow in follows" :key="follow.nickname"></ul>
+      <h3>{{follows.length}}명을 팔로우하고있ㅅ믄다</h3>
+      <!-- 나중에 닉네임 같은 걸로 내용 바꾸기~ -->
+      <li v-for="(follow, idx) in follows" :key="idx">{{follow.to}}</li>
+    </div>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import http from '@/util/http-common';
+import Header from '@/components/layout/header/Header.vue'
+import Footer from '@/components/layout/footer/Footer.vue'
+
+
 
 export default {
     name:'Following',
+    components: {
+      Header,
+      Footer
+    },
     data () { 
       return {
-      // 팔로우수도 추가..?
       followcnt: 0,
       follows: [],
       }
     },
+    created () {
+      this.getUser()
+    },
     methods: {
-      // 팔로잉하는 유저 정보 get
-      // followInfo (uid) {
-        // get(
-          // store..?
-        // ).then(res => {
-          // get으로 가져온 유저들 정보를을 followings에 넣는다.
-          // this.follows = res.data
-
-        // }) 
-      // }
+      getUser () {
+        http.get('/follow/following', {params: {to: 1}}
+        ).then(res => {
+          console.log(res.data)
+          this.follows = res.data
+        }).catch(err => {
+          alert('실패');
+          console.log(err)
+        })
+      }
     }
 }
 </script>
 
 <style>
-
+  @import "../../components/css/user/follow.css"
 </style>
