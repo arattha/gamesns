bebdfbad1f365d12b39a4f0a1b12e5396da07c34
성루비 <quipu1@naver.com>
2021-07-26 16:49:21 +0900

    fix: Frontend 알림창 디자인 수정
    2021.07.26 알림창 헤더, 푸터 추가 및 대략적인 디자인 수정
    
    #S05P12C203-120

---
+++ frontend/src/views/alarm/Alarm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Header/>
+        <div class="form">
+            <h5 class="title">알림 - {{}}개</h5>
+            <div class="list">
+                <li>ddd</li>
+            </div>
+        </div>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/layout/header/Header.vue'
+import Footer from '@/components/layout/footer/Footer.vue'
+
+export default {
+    name: 'Alarm',
+    components: {
+        Header,
+        Footer,
+    },
+}
+</script>
+
+<style>
+    @import "../../components/css/alarm/alarm.css";
+</style>
