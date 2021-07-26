4bd6fe6b42d707e3f8db0454b1d9ff92169dbfb2
chanin <chanin.shim@gmail.com>
2021-07-26 15:52:07 +0900

    feat:header 및 footer 이동기능 추가
    2021.07.26
    
    - header 아이콘 클릭시 이동 기능 추가
    - footer 아이콘 클릭시 이동 기능 추가
    - 아이콘 hover 효과 추가
    - join.vue 디자인 수정
    
    #S05P12C203-114

--- frontend/src/views/feed/Mainfeed.vue
+++ frontend/src/views/feed/Mainfeed.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
       <Header/>
+      <Footer/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/layout/header/Header.vue'
+import Footer from '@/components/layout/footer/Footer.vue'
 
 export default {
     name:'Mainfeed',
     components: {
         Header,
+        Footer,
 
     }
 }
