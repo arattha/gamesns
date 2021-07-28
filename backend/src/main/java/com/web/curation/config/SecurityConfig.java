b52f33ca24b1c1eaa6a239438bf5d160b5bf8ba4
조성표 <pyo7410@gmail.com>
2021-07-28 17:45:20 +0900

    fix: SecurityConfig 수정
    2021.07.28

--- backend/src/main/java/com/web/curation/config/SecurityConfig.java
+++ backend/src/main/java/com/web/curation/config/SecurityConfig.java
@@ -40,12 +40,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-//        web.ignoring()
-//                .antMatchers("/favicon.ico", "/v2/api-docs",
-//                        "/configuration/**", "/swagger*/**",
-//                        "/webjars/**", "/swagger/**");
         web.ignoring()
-        	.antMatchers("/**");
+                .antMatchers("/favicon.ico", "/v2/api-docs",
+                        "/configuration/**", "/swagger*/**",
+                        "/webjars/**", "/swagger/**");
     }
 
     @Override
