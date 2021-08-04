0df974f34bc84ca2c4cd1649bfe68d05071c9107
조성표 <pyo7410@gmail.com>
2021-08-04 14:14:50 +0900

    fix: mypage/edit에서 redirect가 잘못되는 문제 수정

--- frontend/src/components/layout/header/Header.vue
+++ frontend/src/components/layout/header/Header.vue
@@ -1,43 +1,43 @@
 <template>
-    <div class="for-empty-space-header">
+  <div class="for-empty-space-header">
     <div class="main-container" style="padding: 0px;">
-        <nav>
-            <ul class="header" style="padding: 25px 0px 25px 0px">
-                <div class="logo">
-                    <img @click="goToMain" src="@/assets/images/logo2.png" alt="">
-                </div>
-                <div class="alarm-btn">
-                    <div @click="goAlarm"><i class="fas fa-bell"></i></div>
-                </div>
-                <div class="mypage-btn">
-                    <div @click="goToMypage"><i class="fas fa-user-circle" ></i></div>
-                </div>
-                <div class="logout-btn">
-                    <div><i class="fas fa-sign-out-alt"></i></div>
-                </div>
-            </ul>
-        </nav>
+      <nav>
+        <ul class="header" style="padding: 25px 0px 25px 0px">
+          <div class="logo">
+            <img @click="goToMain" src="@/assets/images/logo2.png" alt="" />
+          </div>
+          <div class="alarm-btn">
+            <div @click="goAlarm"><i class="fas fa-bell"></i></div>
+          </div>
+          <div class="mypage-btn">
+            <div @click="goToMypage"><i class="fas fa-user-circle"></i></div>
+          </div>
+          <div class="logout-btn">
+            <div><i class="fas fa-sign-out-alt"></i></div>
+          </div>
+        </ul>
+      </nav>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'Header',
-    methods: {
-        goToMypage: function() {
-            this.$router.push('mypage')
-        },
-        goToMain: function() {
-            this.$router.push('main')
-        },
-        goAlarm: function() {
-            this.$router.push('alarm')
-        }
+  name: 'Header',
+  methods: {
+    goToMypage: function() {
+      this.$router.push('/mypage');
     },
-}
+    goToMain: function() {
+      this.$router.push('/main');
+    },
+    goAlarm: function() {
+      this.$router.push('/alarm');
+    },
+  },
+};
 </script>
 
 <style>
-    @import "../../css/layout/header.css";  
+@import '../../css/layout/header.css';
 </style>
