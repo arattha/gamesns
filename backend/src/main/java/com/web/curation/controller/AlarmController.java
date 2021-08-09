7ff444f5f30a0418858a658b30cf9acd0f9d2388
조용일 <cyi1207@naver.com>
2021-08-09 10:30:47 +0900

    fix: alarm 수정
    2021-08-06
    
    #S05P13C203-59

--- backend/src/main/java/com/web/curation/controller/AlarmController.java
+++ backend/src/main/java/com/web/curation/controller/AlarmController.java
@@ -1,24 +1,38 @@
 package com.web.curation.controller;
 
 import com.web.curation.model.alarm.SocketVO;
+import lombok.Data;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.*;
 
 @Controller
+@CrossOrigin("*")
 @RequestMapping("/alarm")
 public class AlarmController {
 
     // member 별 팔로우 알람을 위한 Map
-    Map<String, Set<String>> FollowAlarm = new HashMap<String, Set<String>>();
+    Map<String, Set<followingUser>> FollowAlarm = new HashMap<>();
+
+    @Data
+    public class followingUser {
+        String uid;
+        String nickname;
+
+        public followingUser(String uid, String nickname) {
+            this.uid = uid;
+            this.nickname = nickname;
+        }
+    }
 
     @MessageMapping("/first")
     @SendTo("/firstSend")
-    public Map<String, Set<String>> first(){
+    public Map<String, Set<followingUser>> first(){
         return FollowAlarm;
     }
 
@@ -30,7 +44,7 @@ public class AlarmController {
 
     // SocketHandler는 1) /receive에서 메시지를 받고, /send로 메시지를 보내줍니다.
     // 정의한 SocketVO를 1) 인자값, 2) 반환값으로 사용합니다.
-    public Map<String, Set<String>> SocketHandler(SocketVO socketVO) {
+    public Map<String, Set<followingUser>> SocketHandler(SocketVO socketVO) {
 
         System.out.println(socketVO);
 
@@ -41,7 +55,7 @@ public class AlarmController {
 
         if(!fromMember.equals("")) {
             // FollowAlarm update
-            updateFollow(fromMember, toMember);
+            updateFollow(socketVO);
         }
 
         System.out.println(FollowAlarm);
@@ -49,26 +63,30 @@ public class AlarmController {
         return FollowAlarm;
     }
 
-    private void updateFollow(String fromMember, String toMember) {
+    private void updateFollow(SocketVO socketVO) {
         // FollowAlarm 에 newMember 가 있으면 해당 리스트를 가져오고 없으면 새로운 리스트 생성
-        Set<String> members = FollowAlarm.get(toMember) != null ? FollowAlarm.get(toMember) : new HashSet<>();
+        Set<followingUser> members = FollowAlarm.get(socketVO.getFollowingName()) != null ? FollowAlarm.get(socketVO.getFollowingName()) : new HashSet<>();
 
         // toMember 를 key로 갖고 있는 Map 이 있다면 팔로우 신청 목록에 fromMember가 있는지 확인한다.
         // (toMember, fromMember) 쌍이 똑같은게 있다면 flag 를 true 로 바꿔주고 Set 에서 삭제한다.
         boolean flag = false;
+        String fromMember = socketVO.getMemberName();
+
+        followingUser fU = new followingUser(socketVO.getUid(), fromMember);
+
         if(members.size() != 0) {
-            for (String u : members) {
-                if(u.equals(fromMember)){
-                    members.remove(fromMember);
+            for (followingUser u : members) {
+                if(u.getNickname().equals(fromMember)){
+                    members.remove(fU);
                     flag = true;
                     break;
                 }
             }
         }
 
-        if(!flag) members.add(fromMember);
+        if(!flag) members.add(fU);
 
-        FollowAlarm.put(toMember, members);
+        FollowAlarm.put(socketVO.getFollowingName(), members);
     }
 
 
--- backend/src/main/java/com/web/curation/model/alarm/SocketVO.java
+++ backend/src/main/java/com/web/curation/model/alarm/SocketVO.java
@@ -10,6 +10,9 @@ public class SocketVO {
     // 유저의 이름을 저장하기 위한 변수
     private String memberName;
 
+    // 유저의 아이디를 저장하기 위한 변수
+    private String uid;
+
     // 팔로잉한 유저의 이름을 저장하기 위한 변수
     private String followingName;
 }
