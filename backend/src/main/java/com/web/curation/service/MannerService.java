52ce57c19ff0c2ba04e7afd4bdacba5ca3182656
성루비 <quipu1@naver.com>
2021-08-10 15:45:57 +0900

    feat: 매너점수
    2021.08.10 매너점수 백엔드 로직 구현
    
    #S05P13C203-75

--- backend/src/main/java/com/web/curation/service/MannerService.java
+++ backend/src/main/java/com/web/curation/service/MannerService.java
@@ -1,4 +1,71 @@
 package com.web.curation.service;
 
+import com.web.curation.dao.member.MemberDao;
+import com.web.curation.model.member.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class MannerService {
+
+    @Autowired
+    MemberDao memberDao;
+
+    public int getManner(String uid) {
+
+        try {
+
+            Optional<Member> optionalMember = memberDao.findMemberByUid(uid);
+
+
+            if (optionalMember.isPresent()) {
+
+                Member member = optionalMember.get();
+
+                int manner = member.getManner();
+
+                return manner;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+        return -1;
+
+    }
+
+    public int addManner(String uid, int score) {
+
+        try {
+
+            Optional<Member> optionalMember = memberDao.findMemberByUid(uid);
+
+            if (optionalMember.isPresent()) {
+
+                Member member = optionalMember.get();
+
+                int manner = member.getManner() + score;
+
+                member.setManner(manner);
+
+                // 어떻게 저장하지,,,
+                memberDao.save(member);
+
+                return manner;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 리턴 2 -> 에러
+            return -1;
+        }
+
+        return -1;
+
+    }
+
 }
