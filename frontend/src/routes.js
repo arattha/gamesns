10b1dfae9a40fa4d09aaa0db4bed110ffb38400a
조성표 <pyo7410@gmail.com>
2021-08-12 12:21:40 +0900

    fix: 프론트가 실행 안되는 문제 및 getCharacterCount로 인해 발생하는 오류 해결
    2021-08-12

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -17,7 +17,6 @@ import UserPage from './views/user/UserPage.vue';
 import Re from './views/user/Re.vue';
 import InChatting from './components/home/chatting/InChatting.vue';
 import Chatting from './components/home/chatting/Chatting.vue';
-import Matching from './components/home/matching/Matching.vue';
 
 export default [
   {
