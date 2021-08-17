5dbd47942809a6994df9d8e57b57fd1cb757806e
조용일 <cyi1207@naver.com>
2021-08-17 10:33:26 +0900

    fix: main.js 경로수정
    2021-08-17
    
    - server.js 소켓 io 통신 url 변경(*)
    
    #S05P13C203-99

--- backend/chat/server.js
+++ backend/chat/server.js
@@ -6,7 +6,7 @@ var app = require("express")();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server, {
     cors: {
-        origin: "http://localhost:3001",
+        origin: "*",
         methods: ["GET", "POST"]
       }
 });
