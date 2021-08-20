a347e063c14a716e0707e43530b60f6da1ee7fb1
조성표 <pyo7410@gmail.com>
2021-08-20 12:07:19 +0900

    fix: 카카오 공유 누락 내용 추가
    2021-08-20

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,51 +4,55 @@
   </div>
 </template>
 <script>
-import "./components/css/style.scss";
-import Stomp from 'webstomp-client'
-import SockJS from 'sockjs-client'
+import './components/css/style.scss';
+import Stomp from 'webstomp-client';
+import SockJS from 'sockjs-client';
 
 export default {
-  name: "app",
-  data(){
-    return{
+  name: 'app',
+  data() {
+    return {
       first: true,
-      recvList:null,
-    }
+      recvList: null,
+    };
   },
   created() {
     this.connect();
   },
   methods: {
     connect() {
-      const serverURL = "http://localhost:8080/alarm"
+      const serverURL = 'http://localhost:8080/alarm';
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
+      this.stompClient.debug = () => {};
 
       this.stompClient.connect(
         {},
-        frame => {
+        (frame) => {
           // 소켓 연결 성공
           this.connected = true;
-          
+
           // 최초 한번 recvList 를 받기 위한 send
-          this.stompClient.send("/receive", JSON.stringify({
-            memberName: "",
-            followingName: "",
-          }), {})
+          this.stompClient.send(
+            '/receive',
+            JSON.stringify({
+              memberName: '',
+              followingName: '',
+            }),
+            {}
+          );
 
           // subscribe 로 alarm List 가져오기
-          this.stompClient.subscribe("/send", (res) => {
+          this.stompClient.subscribe('/send', (res) => {
             this.recvList = JSON.parse(res.body);
-          })
-        
+          });
         },
-        error => {
+        (error) => {
           // 소켓 연결 실패
           console.log('소켓 연결 실패', error);
         }
       );
     },
-  }
+  },
 };
 </script>
